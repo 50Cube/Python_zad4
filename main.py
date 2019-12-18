b9781dbbf2cbba847df111c74231dc3bbe2ab557
Przemysław Komuda <p.komuda1@gmail.com>
2019-12-18 12:44:19 +0100

    dziiik

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
+from tkinter import *
+from sheep import *
+from wolf import *
+
+init_pos_limit = 200
+
+
+def simulate(rounds,
+             sheep_amount,
+             sheep_limit,
+             wolf_distance,
+             sheep_distance):
+    sheeps = []
+    wolf = Wolf(wolf_distance)
+
+    for i in range(sheep_amount):
+        sheeps.append(Sheep(i, sheep_limit, sheep_distance))
+
+    for i in range(rounds):
+        print('Numer tury: ' + str(i+1))
+        print('Liczba owiec: ' + str(len(sheeps)))
+        wolf.print_wolf()
+        if len(sheeps) == 0:
+            print('Wilk zjadł wszystkie owce')
+            break
+        for j in range(len(sheeps)):
+            sheeps[j].move_sheep()
+            # sheeps[j].print_sheep()
+        print()
+        tmp_sheep, tmp_dist = wolf.check_distance(sheeps)
+        wolf.move(tmp_sheep, tmp_dist, sheeps)
+
+
 if __name__ == '__main__':
-    print("HeRb")
+    window = Tk()
+    window.title('HeRb')
+    window.geometry(str(3*init_pos_limit) + 'x' + str(3*init_pos_limit))
+    window.resizable(False, False)
+    window.configure(background='#42F058')
+    window.mainloop()
+    simulate(50, 15, 10.0, 1.0, 0.5)
---
+++ wolf.py
@@ -0,0 +1,47 @@
+import math
+
+
+class Wolf:
+    def __init__(self, distance):
+        self.position = [0.0, 0.0]
+        self.wolf_move_dist = distance
+
+    def print_wolf(self):
+        print('Wilk znajduje sie na pozycji '
+              '[' + '%0.3f' % self.position[0] + ', ' + '%0.3f' % self.position[1] + ']')
+
+    def check_distance(self, sheeps):
+        closest_sheep = sheeps[0]
+        closest_sheep_distance = math.sqrt((sheeps[0].position[0] - self.position[0]) ** 2
+                                           + (sheeps[0].position[1] - self.position[1]) ** 2)
+        for i in range(1, len(sheeps)):
+            if math.sqrt((sheeps[i].position[0] - self.position[0]) ** 2
+                         + (sheeps[i].position[1] - self.position[1]) ** 2) < closest_sheep_distance:
+                closest_sheep = sheeps[i]
+                closest_sheep_distance = math.sqrt((sheeps[i].position[0] - self.position[0]) ** 2
+                                                   + (sheeps[i].position[1] - self.position[1]) ** 2)
+            # print(closest_sheep_distance)
+            # closest_sheep.print_sheep()
+        return closest_sheep, closest_sheep_distance
+
+    def move(self, sheep, distance, sheeps):
+        old_pos = [self.position[0], self.position[1]]
+        if distance < self.wolf_move_dist:
+            print('Wilk zjadl owce nr ' + str(sheep.id + 1) + ' :(')
+            print()
+            self.position[0] = sheep.position[0]
+            self.position[1] = sheep.position[1]
+            sheeps.remove(sheep)
+        else:
+            # print('gonie owce nr ' + str(sheep.id+1))
+            # print()
+            vector = [0.0, 0.0]
+            vector[0] = sheep.position[0] - self.position[0]
+            vector[1] = sheep.position[1] - self.position[1]
+            vector_length = math.sqrt(vector[0] ** 2 + vector[1] ** 2)
+            self.position[0] += (vector[0] / vector_length) * self.wolf_move_dist
+            self.position[1] += (vector[1] / vector_length) * self.wolf_move_dist
+
+    def __str__(self):
+        return 'Wolf(position: ({0}; {1}))'.format('%0.3f' % self.position[0],
+                                                   '%0.3f' % self.position[1])
