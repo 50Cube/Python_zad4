c86c4d179846d35d3467899f6c783ff0a849d8e3
Przemysław Komuda <p.komuda1@gmail.com>
2020-01-08 11:35:04 +0100

    Refactoring

--- globals.py
+++ globals.py
@@ -5,25 +5,6 @@ init_pos_limit = 200
 sheeps = []
 
 
-def on_close():
-    if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
-        window.destroy()
-
-
-sys.stdout = None
-window = Tk()
-window.title('Wolf&Sheep')
-window.geometry(str(3 * init_pos_limit) + 'x' + str(3 * init_pos_limit + 40) + '+500-100')
-window.resizable(False, False)
-window.protocol("WM_DELETE_WINDOW", on_close)
-
-canvas = Canvas(window, width=3 * init_pos_limit, height=3 * init_pos_limit, bg="#42F058")
-canvas.pack()
-
-var = StringVar()
-sheep_label = Label(window, width=init_pos_limit//10, textvariable=var).place(x=2 * init_pos_limit, y=3 * init_pos_limit + init_pos_limit//21)
-
-
 def update_sheep_label():
     var.set('Current sheep amount: ' + str(len(sheeps)))
 
@@ -45,3 +26,21 @@ def create_circle(x, y, r, color):
     x1 = x + r
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1, fill=color, outline='')
+
+
+def on_close():
+    if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+        window.destroy()
+
+
+window = Tk()
+window.title('Wolf&Sheep')
+window.geometry(str(3 * init_pos_limit) + 'x' + str(3 * init_pos_limit + 40) + '+500-100')
+window.resizable(False, False)
+window.protocol("WM_DELETE_WINDOW", on_close)
+
+canvas = Canvas(window, width=3 * init_pos_limit, height=3 * init_pos_limit, bg="#42F058")
+canvas.pack()
+
+var = StringVar()
+sheep_label = Label(window, width=init_pos_limit//10, textvariable=var).place(x=2 * init_pos_limit, y=3 * init_pos_limit + init_pos_limit//21)
